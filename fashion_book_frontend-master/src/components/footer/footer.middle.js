5ffc04eb0408cbdea82622c060d4b442c341a341
nguyendolar <nguyencaonguyencmu@gmail.com>
2021-10-26 16:28:56 +0700

    up

---
+++ fashion_book_frontend-master/src/components/footer/footer.middle.js
@@ -0,0 +1,27 @@
+	import React, { Component } from "react"
+
+	const FooterMiddle = () => (
+		<div className="footer-widget">
+				<div className="container">
+					<div className="row">
+						
+						<div className="col-sm-6">
+							<div className="single-widget">
+								<h2>About Shopper</h2>
+								<form action="#" className="form-footer container">
+									<div className='row'>
+									<input type="text" placeholder="Your email address" className='col-sm-4' />
+									<button type="submit" className=""><i className="fa fa-arrow-circle-o-right col-sm-2"></i>Submit</button>
+									</div>
+								
+									
+								</form>
+								<p>Đăng Ký Gmail để nhận<br />những thống báo mới nhất về chúng tôi...</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+	)
+	export default FooterMiddle
